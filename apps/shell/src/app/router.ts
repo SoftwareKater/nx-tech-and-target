2ef8209a42f182eaabaf60c12d5331507e9533da
SoftwareKater <softwarekater@gmail.com>
2022-06-29 15:07:35 +0200

    add root component with basic routing

---
+++ apps/shell/src/app/router.ts
@@ -0,0 +1,27 @@
+const ROUTES = {
+  '/': 'root',
+  '/app-a': 'app-a',
+  '/app-b': 'app-b',
+  '/app-c': 'app-c',
+  '/app-d': 'app-d',
+};
+
+export function getComponentByPath(path: string): HTMLElement {
+  const elementName = ROUTES[path];
+  const component = document.createElement(elementName);
+  return component;
+}
+
+export function renderComponent(
+  parent: HTMLElement,
+  component: HTMLElement
+): void {
+  parent.appendChild(component);
+}
+
+export function doRouting() {
+  const path = window.location.pathname;
+  console.log(path);
+  const webComponent = getComponentByPath(path);
+  console.log(webComponent);
+}
